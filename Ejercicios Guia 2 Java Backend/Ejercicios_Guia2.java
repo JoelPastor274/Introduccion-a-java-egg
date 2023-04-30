71d98e3dcf02687ddc3d912ceababdfe2385d56c
JoelPastor274 <joel274@live.com.ar>
2023-04-30 17:53:42 -0300

    ejercicios comentados

---
+++ Ejercicios Guia 2 Java Backend/Ejercicios_Guia2.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package egg_backend;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lara
+ */
+public class Ejercicios_Guia2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        ////////////////////////////////////////////////////////
+        
+        //                      EJERCICIO 1
+        // Escribir un programa que pida dos números enteros por teclado y 
+        // calcule la suma de los dos. El programa deberá después mostrar el 
+        // resultado de la suma
+        
+        //Scanner scanner = new Scanner(System.in);
+        //System.out.print("Introduce el primer número: ");
+        //int numero1 = scanner.nextInt();
+        //System.out.print("Introduce el segundo número: ");
+        //int numero2 = scanner.nextInt();
+        //int suma = numero1 + numero2;
+        //System.out.println(suma);
+         
+       ////////////////////////////////////////////////////////////
+       
+       //                       EJERCICIO 2
+       // Escribir un programa que pida tu nombre, lo guarde en una 
+       // variable y lo muestre por pantalla.
+       
+       //Scanner tunombre = new Scanner(System.in);
+       //System.out.print("Ingrese su nombre: ");
+       //String nombre = tunombre.next()
+       //System.out.println(nombre);
+       
+       /////////////////////////////////////////////////////////////
+       
+       //                       EJERCICIO 3
+       // Escribir un programa que pida una frase 
+       // y la muestre toda en mayúsculas y después toda en minúsculas.
+       
+       //Scanner frase = new Scanner(System.in);
+       //System.out.print("Ingrese una frase: ");
+       //String frase2 = frase.nextLine();
+       //String mayus = frase2.toUpperCase();
+       //String minu = frase2.toLowerCase();
+       //System.out.println("\n" + mayus);
+       //System.out.println("\n" + minu);
+       
+       //////////////////////////////////////////////////////////////
+       
+       //                       EJERCICIO 4
+       // Dada una cantidad de grados centígrados se debe mostrar su 
+       // equivalente en grados Fahrenheit. La fórmula correspondiente es: F = 32
+       // + (9 * C / 5).
+       
+       //Scanner grados = new Scanner(System.in);
+       //System.out.print("Ingrese cantidad de grados centigrados: ");
+       //double centigrados = grados.nextDouble();
+       //double faren = 32 + (9*centigrados/5);
+       //System.out.println("El equivalente en Fahrenheit es: "+faren);
+       
+       ////////////////////////////////////////////////////////////////
+       
+       //                       EJERCICIO 5
+       // Escribir un programa que lea un número entero por teclado y 
+       // muestre por pantalla el doble, el triple y la raíz cuadrada de 
+       // ese número. 
+       
+       //Scanner num1 = new Scanner(System.in);
+       //System.out.print("Ingrese numero: ");
+       //int numero = num1.nextInt();
+       //int doble = numero*2;
+       //int triple = numero*3;
+       //double raiz = Math.sqrt(numero);
+       //System.out.println("El doble es: \n"+doble);
+       //System.out.println("El triple es: \n"+triple);
+       //System.out.println("La raiz es: \n"+raiz);
+       
+    }
+    
+}
