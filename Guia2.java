e0bb77dc576e025f2ef396be2260b42377c7a5a1
JoelPastor274 <joel274@live.com.ar>
2023-04-30 17:35:07 -0300

    guia 2

---
+++ Guia2.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package egg_backend;
+
+
+
+/**
+ *
+ * @author lara
+ */
+public class Guia2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //// Tipos de datos ///////
+     	//String nombre;
+  	//boolean bandera;
+  	//char hoja;
+        
+        //////////////////////////////////////
+        
+        //// Declaracion de variables///
+        //  String nombre = "Joel";
+        //  int numero = 25;
+        //  double decimal = 36.3;
+        
+        ///////////////////////////////////////
+        
+        /// Ejercicio 2 de la guia 2 
+        
+        //  int numero = 48;
+        //  double decimales = 3.55; 	
+        //  boolean bandera = false;
+        
+        ////////////////////////////////////////
+        
+        // Ejercicio 3 de la guia 
+        // operadores aritmeticos en variables
+        
+        //  int num1 = 365;
+        //  int num2 = 230;
+        //  int suma = num1 + num2;
+        //  double division = num1 / num2;
+        //  int multiplicacion = num1 * num2;
+        //  boolean logico = num1 < num2;
+       
+        /////////////////////////////////////////
+        
+        // Ejercicio 4 de la guia
+        // mostrar en pantalla textos y variables
+        
+        //  int num1 = 26;
+        //  String nombre = "joel";
+        //  System.out.print("Hola soy "+nombre+" y tengo "+num1+" años ");
+        
+        ///////////////////////////////////////////
+        
+        // Converir un valor numerico (entero) en una cadena de texto
+        
+        //int numEntero = 4;	
+        //String numCadena = String.valueOf(numEntero);
+        //System.out.print(numCadena);
+        
+        // convertir una cadena en un valor numerico (entero)
+        
+        //String numCadena = "1";  	
+        //int numEntero = Integer.parseInt(numCadena);
+        
+        ////////////////////////////////////////////
+       
+    }
+    
+}
